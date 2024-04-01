dd72497b0d9909c61086ad746c3dfab6a9c533c5
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2024-04-02 11:52:36 +1300

    Initial commit

---
+++ rollup.config.ts
@@ -0,0 +1,52 @@
+import rollupPluginReplace from "@rollup/plugin-replace";
+import { rollupPlugin as rollupPluginDeassert } from "deassert";
+import { type RollupOptions } from "rollup";
+import rollupPluginAutoExternal from "rollup-plugin-auto-external";
+import rollupPluginTs from "rollup-plugin-ts";
+
+import pkg from "./package.json" assert { type: "json" };
+
+const treeshake = {
+  annotations: true,
+  moduleSideEffects: [],
+  propertyReadSideEffects: false,
+  unknownGlobalSideEffects: false,
+} satisfies RollupOptions["treeshake"];
+
+const library = {
+  input: "src/index.ts",
+
+  output: [
+    {
+      file: pkg.exports.import,
+      format: "esm",
+      sourcemap: false,
+    },
+    {
+      file: pkg.exports.require,
+      format: "cjs",
+      sourcemap: false,
+    },
+  ],
+
+  plugins: [
+    rollupPluginAutoExternal(),
+    rollupPluginTs({
+      transpileOnly: true,
+      tsconfig: "tsconfig.build.json",
+    }),
+    rollupPluginReplace({
+      values: {
+        "import.meta.vitest": "undefined",
+      },
+      preventAssignment: true,
+    }),
+    rollupPluginDeassert({
+      include: ["**/*.{js,ts}"],
+    }),
+  ],
+
+  treeshake,
+} satisfies RollupOptions;
+
+export default [library];
---
+++ tests/index.test.ts
@@ -0,0 +1,7 @@
+import { expect, describe, it } from "vitest";
+
+describe("noop", () => {
+  it("boop", () => {
+    expect(1).toBe(1);
+  });
+});
